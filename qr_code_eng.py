0531a4997e6f21337b710ca79a4af395d526283c
marjaway <aleks17124@gmail.com>
2025-08-02 18:22:03 +0300

    Add files via upload

---
+++ qr_code_eng.py
@@ -0,0 +1,103 @@
+import segno
+from colorama import init
+from colorama import Fore 
+init()
+
+def main():
+    input("Press Enter to return...")
+
+def returnn():
+    input(Fore.RESET + "\nContinue (Press Enter)\n")
+
+def error():
+    input(Fore.RED + "error... Press Enter to go back\n" + Fore.RESET)
+    
+
+def save():
+    print(Fore.GREEN + "The QR code has been generated. Saved to the folder where the program is located")
+
+
+while True:
+    print('''QR code generator\n                                           
+⠀[ 1 ] - Start generating
+⠀[ 2 ] - Author⠀⠀
+⠀[ 3 ] - Log out
+''')
+    
+    command = input(Fore.CYAN + f"\nSelect a command:{Fore.RESET} ")
+    
+    if command == "1":
+        print(Fore.RESET + '''
+    [ 1 ] - Regular QR code (colors will be: white and black)
+    [ 2 ] - Multicolored QR code\n''')
+        
+        commandqr = input(Fore.CYAN + f"Select a command:{Fore.RESET} ")
+        
+        if commandqr == "1":
+            qrcod = input(Fore.RESET + "\nEnter the link for the QR code: ")
+            qrcode = segno.make_qr(f"{qrcod}")
+
+            print('''\nWhat format should I save it in?
+                1. PNG
+                2. SVG
+                3. PDF''')
+            try:
+                
+                photo = input("\n[ ? ] - Select format: ")
+                nameqr = input("\nEnter a name for the QR code:: ")
+
+                if photo == "1":
+                    qrcode.save(f"{nameqr}.png", scale=10)
+                    save()
+                elif photo == "2":
+                    qrcode.save(f"{nameqr}.svg", scale=10)
+                    save()
+                elif photo == "3":
+                    qrcode.save(f"{nameqr}.pdf", scale=10)
+                    save()
+                else:
+                    print(Fore.RED + "It is necessary to select the format in which the QR code will be saved\n")
+            except ValueError:
+                error()
+        elif commandqr == "2":
+            print(Fore.RESET + '''
+    [ 1 ] - Black
+    [ 2 ] - Grey
+    [ 3 ] - Blue
+    [ 4 ] - Return''')
+            
+            colorqr = input(Fore.CYAN + f"\nSelect a command:{Fore.RESET} ")
+            
+            if colorqr == "1":
+                nameqr = input("Enter a name for the QR code: ")
+                qrcod = input(Fore.RESET + "\nEnter the link for the QR code: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', dark=None, light='black', scale=10)
+                save()
+            elif colorqr == "2":
+                nameqr = input("Enter a name for the QR code: ")
+                qrcod = input(Fore.RESET + "\nEnter the link for the QR code: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', light=None, scale=10)
+                save()
+            elif colorqr == "3":
+                nameqr = input("Enter a name for the QR code: ")
+                qrcod = input(Fore.RESET + "\nEnter the link for the QR code: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', dark='#00fc', scale=10)
+                save()
+            elif colorqr == "4":
+                main()
+            else:
+                error()             
+        else:
+            error()
+        returnn()
+    
+    elif command == "2":
+        print(Fore.RESET + 'Author of the program: marjaway\n')
+        main()
+    elif command == "3":
+        break
+    else:
+        error()
---
+++ qr_code_rus.py
@@ -0,0 +1,103 @@
+import segno
+from colorama import init
+from colorama import Fore 
+init()
+
+def main():
+    input("Нажмите Entert, чтобы вернуться...")
+
+def returnn():
+    input(Fore.RESET + "\nПродолжить (Нажмите Enter)\n")
+
+def error():
+    input(Fore.RED + "Ошибка... Нажмите Enter, чтобы вернуться\n" + Fore.RESET)
+    
+
+def save():
+    print(Fore.GREEN + "QR-код был сгенерирован. Сохраненно в папку где находиться программа")
+
+
+while True:
+    print('''Генератор QR-кодов\n                                           
+⠀[ 1 ] - Начать генерацию⠀
+⠀[ 2 ] - Автор⠀⠀
+⠀[ 3 ] - Выйти
+''')
+    
+    command = input(Fore.CYAN + f"\nВыберете команду:{Fore.RESET} ")
+    
+    if command == "1":
+        print(Fore.RESET + '''
+    [ 1 ] - Обычный QR-код ( цвета будут: белый и черный )
+    [ 2 ] - Разноцветный QR-код\n''')
+        
+        commandqr = input(Fore.CYAN + f"Выберете команду:{Fore.RESET} ")
+        
+        if commandqr == "1":
+            qrcod = input(Fore.RESET + "\nВведите ссылку для QR-кода: ")
+            qrcode = segno.make_qr(f"{qrcod}")
+
+            print('''\nВ каком формате сохранить?
+                1. PNG
+                2. SVG
+                3. PDF''')
+            try:
+                
+                photo = input("\n[ ? ] - Выберете формат: ")
+                nameqr = input("\nВведите название для qr-кода: ")
+
+                if photo == "1":
+                    qrcode.save(f"{nameqr}.png", scale=10)
+                    save()
+                elif photo == "2":
+                    qrcode.save(f"{nameqr}.svg", scale=10)
+                    save()
+                elif photo == "3":
+                    qrcode.save(f"{nameqr}.pdf", scale=10)
+                    save()
+                else:
+                    print(Fore.RED + "Нужно обязательно выбрать формат, в котором сохранится QR-code\n")
+            except ValueError:
+                error()
+        elif commandqr == "2":
+            print(Fore.RESET + '''
+    [ 1 ] - Черный
+    [ 2 ] - Серый
+    [ 3 ] - Синий
+    [ 4 ] - Вернуться''')
+            
+            colorqr = input(Fore.CYAN + f"\nВыберете команду:{Fore.RESET} ")
+            
+            if colorqr == "1":
+                nameqr = input("Введите название для qr-кода: ")
+                qrcod = input(Fore.RESET + "\nВведите ссылку для QR-кода: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', dark=None, light='black', scale=10)
+                save()
+            elif colorqr == "2":
+                nameqr = input("Введите название для qr-кода: ")
+                qrcod = input(Fore.RESET + "\nВведите ссылку для QR-кода: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', light=None, scale=10)
+                save()
+            elif colorqr == "3":
+                nameqr = input("Введите название для qr-кода: ")
+                qrcod = input(Fore.RESET + "\nВведите ссылку для QR-кода: ")
+                qrcode = segno.make_qr(f"{qrcod}")
+                qrcode.save(f'{nameqr}.png', dark='#00fc', scale=10)
+                save()
+            elif colorqr == "4":
+                main()
+            else:
+                error()             
+        else:
+            error()
+        returnn()
+    
+    elif command == "2":
+        print(Fore.RESET + 'Автор программы: marjaway\n')
+        main()
+    elif command == "3":
+        break
+    else:
+        error()
